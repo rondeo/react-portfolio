19b8e7633329512a30f9991b9609b21532107b36
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-05-30 04:25:53 -0700

    Modified components in app.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import PortfolioContainer from './portfolio-container';
+
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Ashley Ferguson's Portfolio</h1>
+        <PortfolioContainer />
 
       </div>
     );
