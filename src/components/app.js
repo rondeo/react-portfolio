d6740ff26df535b17480ed471c3c593ad2f1b9f0
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-05-29 21:07:44 -0700

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import moment from "moment";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Ashley Ferguson's Portfolio</h1>
+
+      </div>
+    );
+  }
+}
