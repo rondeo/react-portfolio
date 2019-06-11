88079ae10839786b397ce51eff6a70b8979b827f
Aferguson17 <ashley.ferguson@sjsu.edu>
2019-06-10 19:02:37 -0700

    added auth page

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,14 +1,67 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    render () {
+    constructor() {
+        super ();
+
+        this.state = {
+            pageTitle: "Welcome to my portfolio",
+            isLoading: false,
+            data: []
+        };
+
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+handleFilter(filter) {
+    this.setState({
+        data: this.state.data.filter(item => {
+            return item.category === filter;
+        })
+    });
+}
+
+getPortfolioItems() {
+    axios
+    .get("https://aferguson.devcamp.space/portfolio/portfolio_items")
+    .then(response => {
+      this.setState({
+          data: response.data.portfolio_items
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(function () {
+    });
+  } 
+    portfolioItems() {
+        return this.state.data.map(item => {
+          return ( 
+            <PortfolioItem key={item.id} item={item} />);
+        });
+    }
+    
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
+
+    render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
+
         return (
-            <div>
-                <h2>Portfolio items go here...</h2>
-                <PortfolioItem />
-            </div>
+              <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
+                <button className="btn" onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
+                <button className="btn" onClick={() => this.handleFilter("Enterprise")}>Enterprise</button>
+
+                {this.portfolioItems()}
+              </div>
         );
     }
 }
